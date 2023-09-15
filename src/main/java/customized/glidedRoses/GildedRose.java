9b97fa62e432ecc2d5f9589503d6d10b008c6277
vaery00 <vipul.aery@bhn.com>
2023-09-15 13:04:38 +0530

    quick fixes leveraging ide

--- src/main/java/customized/glidedRoses/GildedRose.java
+++ src/main/java/customized/glidedRoses/GildedRose.java
@@ -1,43 +1,43 @@
 
 /**
- ======================================
- Gilded Rose Requirements Specification
- ======================================
-
- Hi and welcome to team Gilded Rose. As you know, we are a small inn with a prime location in a
- prominent city ran by a friendly innkeeper named Allison. We also buy and sell only the finest goods.
- Unfortunately, our goods are constantly degrading in quality as they approach their sell by date. We
- have a system in place that updates our inventory for us. It was developed by a no-nonsense type named
- Leeroy, who has moved on to new adventures. Your task is to add the new feature to our system so that
- we can begin selling a new category of items. First an introduction to our system:
-
- - All items have a SellIn value which denotes the number of days we have to sell the item
- - All items have a Quality value which denotes how valuable the item is
- - At the end of each day our system lowers both values for every item
-
- Pretty simple, right? Well this is where it gets interesting:
-
- - Once the sell by date has passed, Quality degrades twice as fast
- - The Quality of an item is never negative
- - "Aged Brie" actually increases in Quality the older it gets
- - The Quality of an item is never more than 50
- - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
- - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
- Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
- Quality drops to 0 after the concert
-
- We have recently signed a supplier of conjured items. This requires an update to our system:
-
- - "Conjured" items degrade in Quality twice as fast as normal items
-
- Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
- still works correctly. However, do not alter the Item class or Items property as those belong to the
- goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
- ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
- for you).
-
- Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
- legendary item and as such its Quality is 80 and it never alters.
+ * ======================================
+ * Gilded Rose Requirements Specification
+ * ======================================
+ * <p>
+ * Hi and welcome to team Gilded Rose. As you know, we are a small inn with a prime location in a
+ * prominent city ran by a friendly innkeeper named Allison. We also buy and sell only the finest goods.
+ * Unfortunately, our goods are constantly degrading in quality as they approach their sell by date. We
+ * have a system in place that updates our inventory for us. It was developed by a no-nonsense type named
+ * Leeroy, who has moved on to new adventures. Your task is to add the new feature to our system so that
+ * we can begin selling a new category of items. First an introduction to our system:
+ * <p>
+ * - All items have a SellIn value which denotes the number of days we have to sell the item
+ * - All items have a Quality value which denotes how valuable the item is
+ * - At the end of each day our system lowers both values for every item
+ * <p>
+ * Pretty simple, right? Well this is where it gets interesting:
+ * <p>
+ * - Once the sell by date has passed, Quality degrades twice as fast
+ * - The Quality of an item is never negative
+ * - "Aged Brie" actually increases in Quality the older it gets
+ * - The Quality of an item is never more than 50
+ * - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+ * - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+ * Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+ * Quality drops to 0 after the concert
+ * <p>
+ * We have recently signed a supplier of conjured items. This requires an update to our system:
+ * <p>
+ * - "Conjured" items degrade in Quality twice as fast as normal items
+ * <p>
+ * Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
+ * still works correctly. However, do not alter the Item class or Items property as those belong to the
+ * goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
+ * ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
+ * for you).
+ * <p>
+ * Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
+ * legendary item and as such its Quality is 80 and it never alters.
  **/
 
 //package com.gildedrose;
@@ -46,58 +46,52 @@ package customized.glidedRoses;
 public class GildedRose {
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+        for (Item item : items) {
+
+            String sulfuras = "Sulfuras, Hand of Ragnaros";
+            String backstage = "Backstage passes to a TAFKAL80ETC concert";
+            String agedBrie = "Aged Brie";
+
+            if (!item.name.equals(agedBrie) && !item.name.equals(backstage)) {
+                if (item.quality > 0 && (!item.name.equals(sulfuras))) {
+                    item.quality = item.quality - 1;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (item.name.equals(backstage)) {
+                        if (item.sellIn < 11 && (item.quality < 50)) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (item.sellIn < 6 && (item.quality < 50)) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(sulfuras)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+            if (item.sellIn < 0) {
+                if (!item.name.equals(agedBrie)) {
+                    if (!item.name.equals(backstage)) {
+                        if (item.quality > 0 && (!item.name.equals(sulfuras))) {
+                            item.quality = item.quality - 1;
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                } else if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
