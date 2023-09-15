3c134d18493cfc1cbc12a135246212a4cd2dbd51
vaery00 <vipul.aery@bhn.com>
2023-09-15 16:56:47 +0530

    refactored if's

--- src/main/java/customized/glidedRoses/GildedRose.java
+++ src/main/java/customized/glidedRoses/GildedRose.java
@@ -127,25 +127,26 @@ public class GildedRose {
                         }
                     }
                 }
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
 
             item.sellIn = item.sellIn - 1;
 
-            if (item.sellIn < 0) {
-                if (item.name.equals(agedBrie)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else if (item.name.equals(backstage)) {
-                    item.quality = 0;
-                } else if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+            if (item.sellIn > 0) {
+                continue;
             }
+
+            if (item.name.equals(agedBrie)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else if (item.name.equals(backstage)) {
+                item.quality = 0;
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+
         }
     }
 }
