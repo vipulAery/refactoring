8f2ec5f3a7cfef1695bb4eda5ce0403f44ad68b2
vaery00 <vipul.aery@bhn.com>
2023-09-15 13:22:59 +0530

    relevant question text seprated for understanding

--- src/main/java/customized/glidedRoses/GildedRose.java
+++ src/main/java/customized/glidedRoses/GildedRose.java
@@ -40,6 +40,27 @@
  * legendary item and as such its Quality is 80 and it never alters.
  **/
 
+
+/*
+<p>
+* Pretty simple, right? Well this is where it gets interesting:
+* <p>
+* - Once the sell by date has passed, Quality degrades twice as fast
+* - The Quality of an item is never negative
+* - "Aged Brie" actually increases in Quality the older it gets
+* - The Quality of an item is never more than 50
+* - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+* - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches
+* Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+* Quality drops to 0 after the concert
+* <p>
+* We have recently signed a supplier of conjured items. This requires an update to our system:
+* <p>
+* - "Conjured" items degrade in Quality twice as fast as normal items
+* <p>
+
+*/
+
 //package com.gildedrose;
 package customized.glidedRoses;
 
